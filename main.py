373ab124c07b9637a98d26be2f027dca6f9b82fa
Oscar <oscar.corral@epitech.eu>
2021-08-12 16:13:37 +0200

    Clean code

--- main.py
+++ main.py
@@ -1,149 +1,160 @@
-import numpy
 import tensorflow as tf
-import tensorflow_transform as tft
-import tensorflow_transform.beam as tft_beam
 import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 import datetime as dt
-import json
+import requests
 
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras import layers
 
 import sentiment_analysis
 
-import requests
-
-# Load Data
-company = 'AAPL'
 
 USER_AGENT = {
     'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
                    ' Chrome/91.0.4472.124 Safari/537.36')
-    }
+}
 sesh = requests.Session()
 sesh.headers.update(USER_AGENT)
 
-# https://github.com/yumoxu/stocknet-dataset
-# https://azure.microsoft.com/en-us/services/cognitive-services/text-analytics/#features
-# https://developer.twitter.com/en/docs/tutorials/how-to-analyze-the-sentiment-of-your-own-tweets
 
+class StockModel:
 
-def load_data(file, start, end):
-    # fetched_data = pdr.DataReader(company, 'av-daily', start, end, api_key='6MD7RC1W9BX3339N')
-    fetched_data = pdr.DataReader(company, 'yahoo', start, end, session=sesh)
-    fetched_data.to_csv(f'data_{file}.csv')
-    return fetched_data
+    def __init__(self, company, prediction_days, plot, train_start, train_end, test_start, test_end):
+        self.company = company
+        self.prediction_days = prediction_days
+        self.plot = plot
+        self.train_start = train_start
+        self.train_end = train_end
+        self.test_start = test_start
+        self.test_end = test_end
 
+        self.model = None
+        self.data = None
+        self.x_train = None
+        self.y_train = None
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.model_inputs = None
 
-def preprocess_tweets():
-    with open('tweets/raw/response-20-21-@Apple.json') as f:
-        data = json.load(f)
-    data = np.array(data['results'])
-    parsed = []
-    for tweet in data:
-        if tweet['lang'] == 'en':
-            parsed.append({
-                'id': tweet['id'],
-                'created_at': dt.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y'),
-                'text': tweet['text']
-            })
-    for t in parsed:
-        print(t)
+    def get_historical_prices(self, file, start, end):
+        try:
+            data = pd.read_csv(f'data_{file}.csv')
+        except IOError:
+            data = pdr.DataReader(self.company, 'yahoo', start, end, session=sesh)
+            data.to_csv(f'data_{file}.csv')
+        return data
+
+    def print_plot(self, actual_prices, predicted_prices):
+        plt.plot(actual_prices, color='black', label=f'Actual {self.company} price')
+        plt.plot(predicted_prices, color='green', label=f'Predicted {self.company} price')
+        plt.title(f"{self.company} Share price")
+        plt.xlabel("Time")
+        plt.ylabel(f"{self.company} Share price")
+        plt.legend()
+        plt.show()
+
+    def train_dataset(self):
+        print(f"Fetching data for {self.company}...\n")
+        data = self.get_historical_prices('train', self.train_start, self.train_end)
+        print(f"Daily closed stock price for {self.company}\n{data['Close']}")
+
+        scaled_data = self.scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+        # Training
+        x_train = []
+        y_train = []
+        for x in range(self.prediction_days, len(scaled_data)):
+            x_train.append(scaled_data[x - self.prediction_days:x, 0])
+            y_train.append(scaled_data[x, 0])
+
+        x_train = np.array(x_train)
+        self.y_train = np.array(y_train)
+        self.x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    def test_dataset(self):
+        # Testing
+        test_data = self.get_historical_prices('test', self.test_start, self.test_end)
+        actual_prices = test_data['Close'].values
+
+        print(f"Actual Data for {self.company}\n{test_data['Close']}")
+        total_dataset = pd.concat((self.data['Close'], test_data['Close']), axis=0)
+
+        model_inputs = total_dataset[len(total_dataset) - len(test_data) - self.prediction_days:].values
+        model_inputs = model_inputs.reshape(-1, 1)
+        self.model_inputs = self.scaler.transform(model_inputs)
+
+        x_test = []
+
+        for x in range(self.prediction_days, len(model_inputs)):
+            x_test.append(model_inputs[x - self.prediction_days:x, 0])
+
+        x_test = np.array(x_test)
+        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+        predicted_prices = self.model.predict(x_test)
+        predicted_prices = self.scaler.inverse_transform(predicted_prices)
+        return actual_prices, predicted_prices
+
+    def process(self):
+        self.train_dataset()
+
+        self.model = tf.keras.Sequential([
+            layers.LSTM(units=50, return_sequences=True, input_shape=(self.x_train.shape[1], 1)),
+            layers.Dropout(0.2),
+            layers.LSTM(units=50, return_sequences=True),
+            layers.Dropout(0.2),
+            layers.LSTM(units=50),
+            layers.Dropout(0.2),
+            layers.Dense(units=1)
+        ])
+
+        self.model.compile(optimizer='adam', loss='mean_squared_error')
+        self.model.fit(self.x_train, self.y_train, epochs=25, batch_size=32)
+
+        actual_prices, predicted_prices = self.test_dataset()
+
+        # Plot
+        if self.plot:
+            self.print_plot(actual_prices, predicted_prices)
+
+        # Predicting
+        real_data = [self.model_inputs[len(self.model_inputs) + 1 - self.prediction_days:len(self.model_inputs + 1), 0]]
+        real_data = np.array(real_data)
+        real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+        prediction = self.model.predict(real_data)
+        prediction = self.scaler.inverse_transform(prediction)
+        print(f"Prediction {prediction}")
 
 
 def main():
-    print(f"Fetching data for {company}...\n")
-    train_start = dt.datetime(2012, 1, 1)
-    train_end = dt.datetime(2020, 1, 1)
-    try:
-        data = pd.read_csv('data_train.csv')
-    except IOError:
-        data = load_data('train', train_start, train_end)
-
-    print(f"Daily closed stock price for {company}\n{data['Close']}")
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
-
-    prediction_days = 60
-
-    x_train = []
-    y_train = []
-
-    for x in range(prediction_days, len(scaled_data)):
-        x_train.append(scaled_data[x - prediction_days:x, 0])
-        y_train.append(scaled_data[x, 0])
-
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
-
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    # Model
-    model = Sequential()
-
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=50, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=50))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1))
-
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, epochs=25, batch_size=32)
-
-    test_start = dt.datetime(2020, 1, 1)
-    test_end = dt.datetime.now()
-    try:
-        test_data = pd.read_csv('data_test.csv')
-    except IOError:
-        test_data = load_data('test', test_start, test_end)
-
-    actual_prices = test_data['Close'].values
-
-    print(f"Actual Data for {company}\n{test_data['Close']}")
-    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
-
-    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
-    model_inputs = model_inputs.reshape(-1, 1)
-    model_inputs = scaler.transform(model_inputs)
-
-    x_test = []
-
-    for x in range(prediction_days, len(model_inputs)):
-        x_test.append(model_inputs[x - prediction_days:x, 0])
-
-    x_test = np.array(x_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-    predicted_prices = model.predict(x_test)
-    predicted_prices = scaler.inverse_transform(predicted_prices)
-
-    # Plot
-    plt.plot(actual_prices, color='black', label=f'Actual {company} price')
-    plt.plot(predicted_prices, color='green', label=f'Predicted {company} price')
-    plt.title(f"{company} Share price")
-    plt.xlabel("Time")
-    plt.ylabel(f"{company} Share price")
-    plt.legend()
-    plt.show()
-
-    real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs + 1), 0]]
-    real_data = np.array(real_data)
-    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-
-    prediction = model.predict(real_data)
-    prediction = scaler.inverse_transform(prediction)
-    print(f"Prediction {prediction}")
+    StockModel(
+        company='AAPL',
+        prediction_days=60,
+        plot=True,
+        train_start=dt.datetime(2012, 1, 1),
+        train_end=dt.datetime(2020, 1, 1),
+        test_start=dt.datetime(2020, 1, 1),
+        test_end=dt.datetime.now()
+    ).process()
 
 
 if __name__ == '__main__':
     # main()
-    # preprocess_tweets()
-    sentiment_analysis.model()
+    sa = sentiment_analysis.SentimentAnalysisModel(
+        batch_size=32,
+        seed=42,
+        max_features=10000,
+        sequence_length=250,
+        embedding_dim=128
+    )
+    sa.process()
+    examples = [
+        "This company is great!",
+        "This company is okay.",
+        "This company is terrible..."
+    ]
+    sa.get_score(examples)
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,121 +1,117 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, SpatialDropout1D
-from tensorflow.keras.layers import Embedding
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 from keras.layers.preprocessing.text_vectorization import LOWER_AND_STRIP_PUNCTUATION
 
-import numpy as np
 import pandas as pd
-import re
-import string
+
 
 # http://help.sentiment140.com/for-students
-# https://developer.twitter.com/en/docs/tutorials/how-to-analyze-the-sentiment-of-your-own-tweets
-
-batch_size = 32
-seed = 42
-max_features = 10000
-sequence_length = 250
-embedding_dim = 128
 
 
-def vectorize_text(text, label):
-    text = tf.expand_dims(text, -1)
-    return vectorize_layer(text), label
+class SentimentAnalysisModel:
 
+    def __init__(self, batch_size, seed, max_features, sequence_length, embedding_dim):
+        self.batch_size = batch_size
+        self.seed = seed
+        self.max_features = max_features
+        self.sequence_length = sequence_length
+        self.embedding_dim = embedding_dim
+        self.vectorize_layer = TextVectorization(
+            standardize=LOWER_AND_STRIP_PUNCTUATION,
+            max_tokens=max_features,
+            output_mode='int',
+            output_sequence_length=sequence_length)
+        self.model = None
+        self.train_ds = None
+        self.val_ds = None
+        self.test_ds = None
 
-vectorize_layer = TextVectorization(
-        standardize=LOWER_AND_STRIP_PUNCTUATION,
-        max_tokens=max_features,
-        output_mode='int',
-        output_sequence_length=sequence_length)
+    def vectorize_text(self, text, label):
+        text = tf.expand_dims(text, -1)
+        return self.vectorize_layer(text), label
 
+    def create_datasets(self):
+        raw_train_ds = pd.read_csv(
+            'text_dataset/training.1600000.processed.noemoticon.csv',
+            encoding="ISO-8859-1",
+            engine='python',
+            names=["Score", "Id", "Date", "Query", "User", "Text"])
+        train_dataset = raw_train_ds.drop(columns=["Id", "Date", "Query", "User"]).sample(frac=1)
+        train_dataset.Score = train_dataset.Score.replace({2: 0.5, 4: 1})
 
-def model():
-    raw_train_ds = pd.read_csv(
-        'text_dataset/training.1600000.processed.noemoticon.csv',
-        encoding="ISO-8859-1",
-        engine='python',
-        names=["Score", "Id", "Date", "Query", "User", "Text"])
-    train_dataset = raw_train_ds.drop(columns=["Id", "Date", "Query", "User"]).sample(frac=1)
-    train_dataset.Score = train_dataset.Score.replace({2: 0.5, 4: 1})
+        train_dataset = train_dataset[:int(len(train_dataset) * 0.04)]
 
-    train_dataset = train_dataset[:int(len(train_dataset) * 0.04)]
+        raw_train_ds = train_dataset[:int(len(train_dataset) * 0.8)]
+        raw_val_ds = train_dataset[int(len(train_dataset) * 0.8):]
 
-    raw_train_ds = train_dataset[:int(len(train_dataset) * 0.8)]
-    raw_val_ds = train_dataset[int(len(train_dataset) * 0.8):]
+        raw_test_ds = pd.read_csv(
+            'text_dataset/testdata.manual.2009.06.14.csv',
+            names=["Score", "Id", "Date", "Query", "User", "Text"])
 
-    raw_test_ds = pd.read_csv(
-        'text_dataset/testdata.manual.2009.06.14.csv',
-        names=["Score", "Id", "Date", "Query", "User", "Text"])
+        test_dataset = raw_test_ds.drop(columns=["Id", "Date", "Query", "User"])
+        test_dataset.Score = test_dataset.Score.replace({2: 0.5, 4: 1})
+        raw_test_ds = test_dataset
 
-    test_dataset = raw_test_ds.drop(columns=["Id", "Date", "Query", "User"])
-    test_dataset.Score = test_dataset.Score.replace({2: 0.5, 4: 1})
-    raw_test_ds = test_dataset
+        raw_train_ds = tf.data.Dataset.from_tensor_slices(
+            (raw_train_ds.Text.values, raw_train_ds.Score.values)).shuffle(len(raw_train_ds)).batch(200)
+        raw_val_ds = tf.data.Dataset.from_tensor_slices(
+            (raw_val_ds.Text.values, raw_val_ds.Score.values)).shuffle(len(raw_val_ds)).batch(200)
+        raw_test_ds = tf.data.Dataset.from_tensor_slices(
+            (raw_test_ds.Text.values, raw_test_ds.Score.values)).batch(20)
 
-    raw_train_ds = tf.data.Dataset.from_tensor_slices((raw_train_ds.Text.values, raw_train_ds.Score.values)).shuffle(len(raw_train_ds)).batch(200)
-    raw_val_ds = tf.data.Dataset.from_tensor_slices((raw_val_ds.Text.values, raw_val_ds.Score.values)).shuffle(len(raw_val_ds)).batch(200)
-    raw_test_ds = tf.data.Dataset.from_tensor_slices((raw_test_ds.Text.values, raw_test_ds.Score.values)).batch(20)
+        train_text = raw_train_ds.map(lambda x, y: x)
+        self.vectorize_layer.adapt(train_text)
 
-    for feat, targ in raw_train_ds.take(3):
-        print('TRAIN Features: {}, Target: {}'.format(feat, targ))
-    for feat, targ in raw_test_ds.take(3):
-        print('TEST Features: {}, Target: {}'.format(feat, targ))
+        self.train_ds = raw_train_ds.map(self.vectorize_text)
+        self.val_ds = raw_val_ds.map(self.vectorize_text)
+        self.test_ds = raw_test_ds.map(self.vectorize_text)
 
-    train_text = raw_train_ds.map(lambda x, y: x)
-    vectorize_layer.adapt(train_text)
+    def process(self):
+        self.create_datasets()
 
-    train_ds = raw_train_ds.map(vectorize_text)
-    val_ds = raw_val_ds.map(vectorize_text)
-    test_ds = raw_test_ds.map(vectorize_text)
+        self.model = tf.keras.Sequential([
+            layers.Embedding(self.max_features + 1, self.embedding_dim),
+            layers.Dropout(0.2),
+            layers.GlobalAveragePooling1D(),
+            layers.Dropout(0.2),
+            layers.Dense(1)])
 
-    model = tf.keras.Sequential([
-        layers.Embedding(max_features + 1, embedding_dim),
-        layers.Dropout(0.2),
-        layers.GlobalAveragePooling1D(),
-        layers.Dropout(0.2),
-        layers.Dense(1)])
+        self.model.summary()
 
-    model.summary()
+        self.model.compile(loss=losses.BinaryCrossentropy(from_logits=True),
+                           optimizer='adam',
+                           metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
 
-    model.compile(loss=losses.BinaryCrossentropy(from_logits=True),
-                  optimizer='adam',
-                  metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+        epochs = 15
+        self.model.fit(
+            self.train_ds,
+            validation_data=self.val_ds,
+            epochs=epochs)
 
-    epochs = 15
-    model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=epochs)
+        loss, accuracy = self.model.evaluate(self.test_ds)
 
-    loss, accuracy = model.evaluate(test_ds)
+        print("Loss: ", loss)
+        print("Accuracy: ", accuracy)
 
-    print("Loss: ", loss)
-    print("Accuracy: ", accuracy)
+        self.model.save('saved_model/sa_model')
 
-    export_model = tf.keras.Sequential([
-        vectorize_layer,
-        model,
-        layers.Activation('sigmoid')
-    ])
+        self.model = tf.keras.Sequential([
+            self.vectorize_layer,
+            self.model,
+            layers.Activation('sigmoid')
+        ])
 
-    export_model.compile(
-        loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
-    )
+        self.model.compile(
+            loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
+        )
 
-    loss, accuracy = export_model.evaluate(raw_test_ds)
-    print(accuracy)
+    def load_model(self):
+        self.model = tf.keras.models.load_model('saved_model/sa_model')
+        self.model.summary()
 
-
-    examples = [
-        "This company is great!",
-        "This company is okay.",
-        "This company is terrible..."
-    ]
-    res = export_model.predict(examples, verbose=1)
-    print(res)
+    def get_score(self, text):
+        res = self.model.predict(text, verbose=1)
+        print(res)
+        return res
